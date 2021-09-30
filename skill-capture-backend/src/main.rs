f38779b123e3f03f7ee3afb8174a19182a9e2be9
Stefan <stefi@sprintingsoftware.com>
2021-09-30 16:12:18 +0200

    add database connection

--- skill-capture-backend/src/main.rs
+++ skill-capture-backend/src/main.rs
@@ -1,9 +1,15 @@
-#[macro_use] extern crate rocket;
+#[macro_use] 
+extern crate rocket;
+extern crate diesel;
+extern crate dotenv;
+use diesel::pg::PgConnection;
+use diesel::Insertable;
 use rocket::serde::json::{Json, Value, json};
 use rocket::serde::{Serialize, Deserialize};
 // use chrono::{DateTime, Duration, Utc};
 
-extern crate dotenv;
+pub mod schema;
+use schema::employee;
 use dotenv::dotenv;
 use std::env;
 
@@ -29,23 +35,36 @@ struct RatedSkill {
     skill: Skill
 }
 
-#[derive(Serialize, Deserialize)]
+
+#[derive(Serialize, Deserialize, Insertable)]
 #[serde(crate = "rocket::serde")]
+#[table_name = "employee"]
 struct Employee {
-    firstName: String,
-    lastName: String,
-    title: String,
-    assignedSkills: Vec<RatedSkill>
+    firstname: String,
+    lastname: String,
+    title: String
 }
+
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+
 #[launch]
 fn rocket() -> _ {
     dotenv().ok();
-
+    let connection = establish_connection();
+    let new_employee = Employee{firstname:String::from("aa"), lastname:String::from("zzz"), title: String::from("xxxzzx")};
+    diesel::insert_into(employee::table).values(&new_employee).execute(&connection);
     rocket::build().mount("/", routes![index])
+}
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection.establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
