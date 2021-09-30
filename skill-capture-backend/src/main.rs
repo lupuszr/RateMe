6d72c5f8c615bfe561fdfba9e22e52e53c3f9d42
Viktor Pelle <viktor@Viktors-MacBook-Pro.local>
2021-09-30 13:20:28 +0200

    Add default model

--- skill-capture-backend/src/main.rs
+++ skill-capture-backend/src/main.rs
@@ -1,4 +1,38 @@
 #[macro_use] extern crate rocket;
+use rocket::serde::json::{Json, Value, json};
+use rocket::serde::{Serialize, Deserialize};
+// use chrono::{DateTime, Duration, Utc};
+
+// todo use chrono
+type DateTime = String;
+type Score = i8;
+type Id = usize;
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Skill {
+    id: Id,
+    name: String,
+    category: String
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct RatedSkill {
+    score: Score,
+    whoRated: Employee,
+    createdAt: DateTime,
+    skill: Skill
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Employee {
+    firstName: String,
+    lastName: String,
+    title: String,
+    assignedSkills: Vec<RatedSkill>
+}
 
 #[get("/")]
 fn index() -> &'static str {
