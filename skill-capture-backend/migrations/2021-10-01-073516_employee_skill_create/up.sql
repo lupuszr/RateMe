8d3ab7c7375c753cd5d000ad67e7bda958b3b1bf
Stefan <stefi@sprintingsoftware.com>
2021-10-01 10:52:59 +0200

    define PK FK constraints in composite table

--- skill-capture-backend/migrations/2021-10-01-073516_employee_skill_create/up.sql
+++ skill-capture-backend/migrations/2021-10-01-073516_employee_skill_create/up.sql
@@ -3,11 +3,7 @@ CREATE TABLE EmployeeSkill (
   skill_id integer NOT NULL,
   employee_id integer NOT NULL,
   history json,
-  CONSTRAINT PK_Employee_Skill_employeeid_skillid PRIMARY KEY (employee_id, skill_id)
-  -- CONSTRAINT FK_Employee_employeeid FOREIGN KEY (employee_id)
-  -- CONSTRAINT FK_Skill_skillid FOREIGN KEY (skill_id)
-  -- CONSTRAINT "FK_EmployeeSkill_employeeid" FOREIGN KEY ("employee_id") REFERENCES "Employee"("id"),
-  -- CONSTRAINT "FK_EmployeeSkill_skillid" FOREIGN KEY ("skill_id") REFERENCES "Skill"("id")
+  CONSTRAINT PK_Employee_Skill_employeeid_skillid PRIMARY KEY (employee_id, skill_id),
+  CONSTRAINT FK_Employee_employeeid FOREIGN KEY (employee_id) REFERENCES Employee,
+  CONSTRAINT FK_Skill_skillid FOREIGN KEY (skill_id) REFERENCES Skill
 )
-  -- FOREIGN KEY (employee_id),
-  -- FOREIGN KEY (skill_id)
